1745a3d48a2f3d543b88db05c3c2ef3f2df3922e
kirikosd <kirikosdoumanis@gmail.com>
2024-02-03 15:35:12 +0200

    fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#index'
-end
 
-devise_scope :user do
-  get 'login', to: 'devise/sessions#new'
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 end
