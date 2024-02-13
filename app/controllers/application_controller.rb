939c09dc2cc6bb35eb3a438ae7f4f043888ef933
kirikosd <kirikosdoumanis@gmail.com>
2024-02-13 20:09:45 +0200

    Render a private conversation window on the app - Add opened conversations to the session - Create a _conversation.html.erb file inside private/conversations - Define a private_conv_recipient helper method in the private/conversations_helper.rb - Define an opposed_user method in Private::Conversation model and add specs for it - Create _heading.html.erb and _messages_list.html.erb files inside the private/conversations/conversation - Define a load_private_messages in private/conversations_helper.rb and add specs for it - Create a _new_message_form.html.erb inside the private/conversations/conversation - Create a _open.js.erbinside private/conversations - Create a  position_and_visibility.js inside the assets/javascripts/conversations - Create a  conversation_window.scss inside the assets/stylesheets/partials - Define an opened_conversations_windows helper method in ApplicationController - Create a _private_conversations_windows.html.erb inside the layouts/application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+    before_action :opened_conversations_windows
+
+    def opened_conversations_windows
+        if user_signed_in?
+          # opened conversations
+          session[:private_conversations] ||= []
+          @private_conversations_windows = Private::Conversation.includes(:recipient, :messages)
+                                            .find(session[:private_conversations])
+        else
+          @private_conversations_windows = []
+        end
+    end
     
     def redirect_if_not_signed_in
         redirect_to root_path if !user_signed_in?
